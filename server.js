ba3fa593c7280a55dfb5f29dce258ba3f0daef2c
callumdmay <callumdmay@gmail.com>
2018-12-02 14:47:58 -0800

    Run eslint on server.js

--- server.js
+++ server.js
@@ -1,39 +1,42 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3002,
-    mongoose = require('mongoose'),
-    Users = require('./models/users'),
-    bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3002;
+
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 
-//var connectionString = require('./local');
-var connectionString = "mongodb://localhost:27017/HeyMentor";
-mongoose.connect(connectionString, { useNewUrlParser: true });
+// var connectionString = require('./local');
+const connectionString = 'mongodb://localhost:27017/HeyMentor';
+mongoose.connect(connectionString, {useNewUrlParser: true});
 
-//var connectionString = process.env.CONNECTION_STRING;
-//mongoose.connect(connectionString);
+// var connectionString = process.env.CONNECTION_STRING;
+// mongoose.connect(connectionString);
 
 // Handle the connection event
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function() {
-    console.log("DB connection alive");
-    var Admin = mongoose.mongo.Admin;
+    console.log('DB connection alive');
+    const Admin = mongoose.mongo.Admin;
     new Admin(db.db).listDatabases(function(err, result) {
         console.log('listDatabases succeeded');
-        var allDatabases = result.databases;
+        const allDatabases = result.databases;
         console.log(allDatabases);
     });
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var routes = require('./routes/routes'); //importing route
-routes(app); //register the route
+const routes = require('./routes/routes'); // importing route
+routes(app); // register the route
 
 app.listen(port);
 
