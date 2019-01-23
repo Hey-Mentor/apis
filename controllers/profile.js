db89a8e8cf3b9c5184073bfb5ac0f7c9fd7e2b18
callumdmay <callumdmay@gmail.com>
2019-01-22 18:47:37 -0800

    Remove mentor/mentee difference from getContacts, use async/await for tests

--- controllers/profile.js
+++ controllers/profile.js
@@ -12,6 +12,7 @@ const PUBLIC_CONTACT_SCHEMA = {
     gen_interest: 1,
     spec_interests: 1,
     sports: 1,
+    school: 1,
 };
 
 exports.getProfileData = function (req, res) {
@@ -19,30 +20,11 @@ exports.getProfileData = function (req, res) {
 };
 
 exports.getContacts = function (req, res) {
-    if (req.user.user_type === 'mentor') {
-        Users.findById(req.user._id, { contacts: 1 })
-            .populate('contacts', Object.assign({}, PUBLIC_CONTACT_SCHEMA, { school: 1 }))
-            .then((result) => {
-                result.contacts = result.contacts.map((contact) => {
-                    if (contact.user_type === 'mentor') {
-                        contact.school = undefined;
-                    }
-                    return contact;
-                });
-                return result;
-            })
-            .then(contacts => res.json(contacts))
-            .catch((err) => {
-                logger.error(err);
-                res.status(500).json({ Error: 'Something went wrong' });
-            });
-    } else {
-        Users.findById(req.user._id, { contacts: 1 })
-            .populate('contacts', PUBLIC_CONTACT_SCHEMA)
-            .then(contacts => res.json(contacts))
-            .catch((err) => {
-                logger.error(err);
-                res.status(500).json({ Error: 'Something went wrong' });
-            });
-    }
+    Users.findById(req.user._id, { contacts: 1 })
+        .populate('contacts', PUBLIC_CONTACT_SCHEMA)
+        .then(contacts => res.json(contacts))
+        .catch((err) => {
+            logger.error(err);
+            res.status(500).json({ Error: 'Something went wrong' });
+        });
 };
--- tests/endpoint_test.js
+++ tests/endpoint_test.js
@@ -28,87 +28,41 @@ describe('API', function () {
         });
     });
 
-    describe('/GET Bad API key', function () {
-        it('should reject an incorrect api key', function () {
-            return request.get(`/profile/${process.env.TEST_MENTOR_USER_ID}?token=${process.env.TEST_MENTEE_API_KEY}`)
-                .then((res) => {
-                    assert.equal(res.status, 401);
-                    assert.isEmpty(res.body);
-                })
-                .catch((err) => {
-                    assert.fail(err);
-                });
+    describe('/GET profile', async function () {
+        it('should GET a users profile', async function () {
+            const res = await request.get(`/profile/${process.env.TEST_MENTOR_USER_ID}?token=${process.env.TEST_MENTOR_API_KEY}`);
+            assert.equal(res.status, 200);
+            assert.typeOf(res.body, 'object');
+            assert.typeOf(res.body.contacts, 'array');
+            assert.typeOf(res.body._id, 'string');
+            assert.exists(res.body.facebook_id || res.body.google_id);
+        });
+
+        it('should reject an incorrect api key', async function () {
+            const res = await request.get(`/profile/${process.env.TEST_MENTOR_USER_ID}?token=${process.env.TEST_MENTEE_API_KEY}`);
+            assert.equal(res.status, 401);
+            assert.isEmpty(res.body);
+        });
+
+        it('should reject a non-existent user', async function () {
+            const res = await request.get(`/profile/123456789123456789123456?token=${process.env.TEST_MENTOR_API_KEY}`);
+            assert.equal(res.status, 401);
+            assert.isEmpty(res.body);
         });
     });
 
-    describe('/GET Bad User ID', function () {
-        it('should reject a non-existent user', function () {
-            return request.get(`/profile/123456789123456789123456?token=${process.env.TEST_MENTOR_API_KEY}`)
-                .then((res) => {
-                    assert.equal(res.status, 401);
-                    assert.isEmpty(res.body);
-                })
-                .catch((err) => {
-                    assert.fail(err);
-                });
-        });
-    });
-
-    describe('/GET profile', function () {
-        it('should GET a users profile', function () {
-            return request.get(`/profile/${process.env.TEST_MENTOR_USER_ID}?token=${process.env.TEST_MENTOR_API_KEY}`)
-                .then((res) => {
-                    assert.equal(res.status, 200);
-                    assert.typeOf(res.body, 'object');
-                    assert.typeOf(res.body.contacts, 'array');
-                    assert.typeOf(res.body._id, 'string');
-                    assert.exists(res.body.facebook_id || res.body.google_id);
-                })
-                .catch((err) => {
-                    assert.fail(err);
-                });
-        });
-    });
 
     describe('/GET contacts', function () {
-        it('should GET a mentor users contacts', function () {
-            return request.get(`/contacts/${process.env.TEST_MENTOR_USER_ID}?token=${process.env.TEST_MENTOR_API_KEY}`)
-                .then((res) => {
-                    assert.equal(res.status, 200);
-                    assert.typeOf(res.body, 'object');
-                    assert.typeOf(res.body.contacts, 'array');
-                    assert.notExists(res.body.contacts[0].api_key);
-                    assert.typeOf(res.body._id, 'string');
-                    res.body.contacts.forEach((contact) => {
-                        if (contact.user_type === 'mentor') {
-                            assert.hasAllKeys(contact, ['_id', 'person', 'demo', 'gen_interest', 'spec_interests', 'user_type', 'sports']);
-                        } else {
-                            assert.hasAllKeys(contact, ['_id', 'person', 'demo', 'gen_interest', 'spec_interests', 'user_type', 'sports', 'school']);
-                        }
-                    });
-                })
-                .catch((err) => {
-                    assert.fail(err);
-                });
-        });
-    });
-
-    describe('/GET contacts', function () {
-        it('should GET a mentee users contacts', function () {
-            return request.get(`/contacts/${process.env.TEST_MENTEE_USER_ID}?token=${process.env.TEST_MENTEE_API_KEY}`)
-                .then((res) => {
-                    assert.equal(res.status, 200);
-                    assert.typeOf(res.body, 'object');
-                    assert.typeOf(res.body.contacts, 'array');
-                    assert.notExists(res.body.contacts[0].api_key);
-                    assert.typeOf(res.body._id, 'string');
-                    res.body.contacts.forEach((contact) => {
-                        assert.hasAllKeys(contact, ['_id', 'person', 'demo', 'gen_interest', 'spec_interests', 'user_type', 'sports'], contact);
-                    });
-                })
-                .catch((err) => {
-                    assert.fail(err);
-                });
+        it('should GET a users contacts', async function () {
+            const res = await request.get(`/contacts/${process.env.TEST_MENTEE_USER_ID}?token=${process.env.TEST_MENTEE_API_KEY}`);
+            assert.equal(res.status, 200);
+            assert.typeOf(res.body, 'object');
+            assert.typeOf(res.body.contacts, 'array');
+            assert.notExists(res.body.contacts[0].api_key);
+            assert.typeOf(res.body._id, 'string');
+            res.body.contacts.forEach((contact) => {
+                assert.hasAllKeys(contact, ['_id', 'person', 'demo', 'gen_interest', 'spec_interests', 'user_type', 'sports', 'school'], contact);
+            });
         });
     });
 });
