327ccf6880c8d769a785e2de4b7d6db21e2c5543
Matt Bongiovi <bongiovimatthew@gmail.com>
2019-01-23 22:01:18 -0800

    Fix my eslint errors

--- controllers/profile.js
+++ controllers/profile.js
@@ -12,19 +12,34 @@ const PUBLIC_CONTACT_SCHEMA = {
     gen_interest: 1,
     spec_interests: 1,
     sports: 1,
+    contacts: 1,
     school: 1,
 };
 
 exports.getProfileData = function (req, res) {
+    logger.info('getProfileData');
     return res.status(200).json(req.user);
 };
 
 exports.getContacts = function (req, res) {
+    logger.info('Getting contacts');
     Users.findById(req.user._id, { contacts: 1 })
         .populate('contacts', PUBLIC_CONTACT_SCHEMA)
+        .then((result) => {
+            result.contacts = result.contacts.map((contact) => {
+                if (contact.contacts.indexOf(req.user._id) < 0) {
+                    logger.info('This user should not be returned');
+                    return null;
+                }
+                return contact;
+            });
+            return result;
+        })
         .then(contacts => res.json(contacts))
         .catch((err) => {
+            logger.info('An error occurred while processing a getContacts request');
             logger.error(err);
+            logger.error(err.stack);
             res.status(500).json({ Error: 'Something went wrong' });
         });
 };
