89b9f87667ba826493027966ff6b4acf4c8fd137
callumdmay <callumdmay@gmail.com>
2019-02-17 10:31:41 -0800

    Add tests, slightly refactor tests

--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
+const authTests = require('./auth_test');
+const chatTests = require('./chat_test');
 const contactTests = require('./contact_test');
 const credentialControllerTests = require('./credentials_test');
 const db_util = require('../tools/db_util');
@@ -19,10 +21,11 @@ describe('API Tests', function () {
         db_util.populateDB()
             .then(() => done());
     });
-
+    describe('Authorization', authTests.bind(this));
     describe('Credential Controller', credentialControllerTests.bind(this));
     describe('Routes', function () {
         describe('/profile', profileTests.bind(this));
         describe('/contacts', contactTests.bind(this));
+    	describe('/chat', chatTests.bind(this));
     });
 });
