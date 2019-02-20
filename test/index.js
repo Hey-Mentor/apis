ef31aee505179e52f5b2cea73b70dcd9f6c628d2
callumdmay <callumdmay@gmail.com>
2019-02-19 21:14:28 -0800

    Change register to credential controller, add route, tests

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const contactTests = require('./contact_test');
+const credentialControllerTests = require('./credentials_test');
 const db_util = require('../tools/db_util');
 const profileTests = require('./profile_test');
 
@@ -13,14 +14,15 @@ mongoose.connect(process.env.TEST_DB_URL, { useNewUrlParser: true });
 /* eslint-disable prefer-arrow-callback */
 
 describe('API Tests', function () {
-    before(function (done) {
+    beforeEach(function (done) {
         // Before running suite we populate the db
-        mongoose.connection.once('open', () => {
-            db_util.populateDB()
-                .then(() => done());
-        });
+        db_util.populateDB()
+            .then(() => done());
     });
 
-    describe('/profile', profileTests.bind(this));
-    describe('/contacts', contactTests.bind(this));
+    describe('Credential Controller', credentialControllerTests.bind(this));
+    describe('Routes', function () {
+        describe('/profile', profileTests.bind(this));
+        describe('/contacts', contactTests.bind(this));
+    });
 });
