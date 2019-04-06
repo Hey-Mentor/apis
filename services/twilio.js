9d14dba74edc7c5a1669fd30204cacbd6e324d5d
callumdmay <callumdmay@gmail.com>
2019-04-06 13:33:41 -0700

    Add test env vars

--- services/twilio.js
+++ services/twilio.js
@@ -10,16 +10,24 @@ exports.TokenGenerator = function (identity, deviceID) {
     // Create a "grant" which enables a client to use Chat as a given user,
     // on a given device
     const chatGrant = new ChatGrant({
-        serviceSid: process.env.TWILIO_CHAT_SERVICE_SID,
+        serviceSid: process.env.NODE_ENV === 'production'
+            ? process.env.TWILIO_CHAT_SERVICE_SID
+            : process.env.TEST_TWILIO_CHAT_SERVICE_SID,
         endpointId,
     });
     // Create an access token which we will sign and return to the client,
     // containing the grant we just created
-    const token = new AccessToken(
-        process.env.TWILIO_ACCOUNT_SID,
-        process.env.TWILIO_API_KEY,
-        process.env.TWILIO_API_SECRET,
-    );
+    const token = process.env.NODE_ENV === 'production'
+        ? new AccessToken(
+            process.env.TWILIO_ACCOUNT_SID,
+            process.env.TWILIO_API_KEY,
+            process.env.TWILIO_API_SECRET,
+        )
+        : new AccessToken(
+            process.env.TEST_TWILIO_ACCOUNT_SID,
+            process.env.TEST_TWILIO_API_KEY,
+            process.env.TEST_TWILIO_API_SECRET,
+        );
     token.addGrant(chatGrant);
     token.identity = String(identity);
 
