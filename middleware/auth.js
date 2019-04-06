8a6f0f0ed73d655aa76437ee1b48eb5538613fde
callumdmay <callumdmay@gmail.com>
2019-04-05 18:34:54 -0700

    Fix lint error

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,8 @@ const { logger } = require('../logging/logger');
 const User = mongoose.model('User');
 
 exports.authorize = function (req, res, next) {
-    User.findOne({ _id: req.params.userId, api_key: req.query.token }, { api_key: 0 }).orFail(new Error())
+    User.findOne({ _id: req.params.userId, api_key: req.query.token }, { api_key: 0 })
+        .orFail(new Error())
         .then((user) => {
             req.user = user;
             return next();
