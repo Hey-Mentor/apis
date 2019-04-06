13deb293ce1992939c034118af55695232d34219
callumdmay <callumdmay@gmail.com>
2019-04-05 18:33:37 -0700

    Add fail on document not found for all mongoose queries

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,11 +5,8 @@ const { logger } = require('../logging/logger');
 const User = mongoose.model('User');
 
 exports.authorize = function (req, res, next) {
-    User.findOne({ _id: req.params.userId, api_key: req.query.token }, { api_key: 0 })
+    User.findOne({ _id: req.params.userId, api_key: req.query.token }, { api_key: 0 }).orFail(new Error())
         .then((user) => {
-            if (!user) {
-                return res.status(401).send('Unauthorized');
-            }
             req.user = user;
             return next();
         }).catch((err) => {
