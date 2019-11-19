ea2900946ecd63aa9479a1b99155a42f93310103
Fredrik Forsell <fredrik.forsell@outlook.com>
2019-11-18 19:43:36 -0800

    Added class for channel modification.
    + DeleteChannel()
    + inviteToChannel()
    
    + createChannel()
       - If the channel exists, it will send new invites to the users.

--- controllers/chat.js
+++ controllers/chat.js
@@ -2,6 +2,7 @@ const Twilio = require('twilio-chat');
 
 const TwilioService = require('../services/twilio');
 
+
 exports.createToken = function (req, res) {
     if (!req.body.device) {
         return res.status(400).send('Missing device');
@@ -14,41 +15,96 @@ exports.createToken = function (req, res) {
     });
 };
 
-exports.createTwilioChannel = async function (req, res){
+
+
+exports.createTwilioChannel = async function (req, res) {
     //Require body to contain channelname
     if (!req.body.channelName) {
-        return res.status(400).send('Missing channelName');
+        return res.status(400).send('The body does not contain a channel name');
     }
 
-    try{
-        const chat_token = TwilioService.TokenGenerator(req.user._id, 'init');
-        const chatClient = await Twilio.Client
-        .create(
-            chat_token, 
-            { logLevel: 'info' }
-        );
-
-        if (!chatClient){
-            return res.sendStatus(501);
+    class Channel {
+        constructor() {
+            //Creating public 
+            this.accountSid = process.env.TEST_TWILIO_ACCOUNT_SID;
+            this.serviceSid = process.env.TEST_TWILIO_CHAT_SERVICE_SID;
+            this.authToken = process.env.TEST_TWILIO_AUTH_TOKEN;
+            this.client = require('twilio')(this.accountSid, this.authToken);
         }
 
-        const channel = await chatClient
-            .createChannel({
-                uniqueName: req.body.channelName,
-                friendlyName: 'Chat channel',
-            }).catch((e) =>{
-                //e.message says why the channel creation failed.
-                return res.status(506).json({ status: 'Failed to create channel. ' + e.message});
-            });
-        
-        console.log('Created new channel.');
+        //code for updating channel info
+        async updateChannelData(channel_sid) {
+            this.client.chat.services(this.serviceSid)
+                .channels(channel_sid)
+                .update({ 'friendlyName': 'Chatroom' })
+                .then(channel => console.log("Successfully changed the friendly channel name to: " + channel.friendlyName));
+        }
 
-    }catch (err) {
+        //code for deleting channels
+        async deleteChannel(channel_sid) {
+            this.client.chat.services(this.serviceSid)
+                .channels(channel_sid)
+                .remove()
+                .then(function (channel) {
+                    console.log("Deleted channel: " + channel_sid);
+                });
+        }
+        //code for inviting to channels
+        async inviteToChannel(channel_sid, identity) {
+                .channels(channel_sid)
+                .invites
+                .create({ identity: identity })
+                .then(function (invite) {
+                    console.log("Invited user: " + invite.sid + " to channel: " + channel_sid);
+                });
+        }
+
+        //code for creating channels
+        async createChannel(channelName) {
+            const newChannel = await this.client.chat.services(this.serviceSid)
+                .channels
+                .create({
+                    uniqueName: channelName,
+                    friendlyName: channelName,
+                })
+                .catch((error) => {
+                    //e.message says why the channel creation failed.
+                    console.log('Failed to create channel. ' + error.message + ". Error: " + error.code);
+
+                    //Catch channel already exists
+                    if (error.code === 50307) {
+                        //TODO: Try to send invite to both users
+                        console.log("Inviting users to: " + channelName);
+                        this.inviteToChannel(channelName, '5c15446bbf35ae4057111111');
+                        this.inviteToChannel(channelName, '5c15446bbf35ae4057222222');
+                    }
+
+                    //Returning 5xx error
+                    return res.status(501).json({ status: 'Failed to create channel. ' + error.message + ". Error: " + error.code });
+                });
+
+                if(newChannel){
+                    return res.status(201).json({ status: 'Twilio channel created' });
+                }
+        }
+    }
+
+
+    //updateChannelData();
+    //deleteChannel('CH0534d4a8a5384aa9a0e204d892c27b8a');
+
+    test = new Channel();
+    //test.updateChannelData('CHc71753c96f8545d6a99d0e054cc48485');
+    //test.inviteToChannel('CH8a8d7a942f594416a080ccbb7a809e74', '5c15446bbf35ae4057111111');
+    //test.inviteToChannel('CH8a8d7a942f594416a080ccbb7a809e74', '5c15446bbf35ae4057222222');
+    
+    try {
+        return test.createChannel(req.body.channelName);
+    } catch (err) {
         return res.sendStatus(500);
     }
 
-    return res.status(201).json({ status: 'Twilio channel created'});
-
+    
 }
 
 
@@ -61,6 +117,17 @@ exports.createTwilioUser = async function (req, res) {
         if (!client) {
             return res.sendStatus(500);
         }
+        this.client.getPublicChannelDescriptors().then((paginator) => {
+            console.log('-        Amount of channels: ' + paginator.items.length);
+
+            for (i = 0; i < paginator.items.length; i++) {
+                const channel = paginator.items[i];
+                if (channel.friendlyName === channelName) {
+                    console.log("-Channel already exist");
+                    return channel.getChannel();
+                }
+            }
+        });
     } catch (err) {
         return res.sendStatus(500);
     }
