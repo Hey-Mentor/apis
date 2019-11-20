e8f237ca0104bb33ddab0a23bba00214ddd19670
Fredrik Forsell <fredrik.forsell@outlook.com>
2019-11-19 22:13:52 -0800

    Added function for sending invites
    + If the request body contains a userList, send channel invitations.
       - If the invitation fails, it is shown in the console.

--- controllers/chat.js
+++ controllers/chat.js
@@ -36,7 +36,9 @@ exports.createTwilioChannel = async function (req, res) {
         async updateChannelData(channel_sid) {
             this.client.chat.services(this.serviceSid)
                 .channels(channel_sid)
-                .update({ 'friendlyName': 'Chatroom' })
+                .update({
+                    'friendlyName': 'Chatroom'
+                })
                 .then(channel => console.log("Successfully changed the friendly channel name to: " + channel.friendlyName));
         }
 
@@ -55,19 +57,23 @@ exports.createTwilioChannel = async function (req, res) {
             this.client.chat.services(this.serviceSid)
                 .channels(channel_sid)
                 .invites
-                .create({ identity: identity })
+                .create({
+                    identity: identity
+                })
                 .then(function (invite) {
                     console.log("Invited user: " + invite.sid + " to channel: " + channel_sid);
+                }).catch(() => {
+                    console.log("Failed to invite user: " + identity + " to channel: " + channel_sid);
                 });
         }
 
         //code for creating channels
-        async createChannel(channelName) {
+        async createChannel() {
             const newChannel = await this.client.chat.services(this.serviceSid)
                 .channels
                 .create({
-                    uniqueName: channelName,
-                    friendlyName: channelName,
+                    uniqueName: req.body.channelName,
+                    friendlyName: req.body.channelName,
                 })
                 .catch((error) => {
                     //e.message says why the channel creation failed.
@@ -76,20 +82,39 @@ exports.createTwilioChannel = async function (req, res) {
                     //Catch channel already exists
                     if (error.code === 50307) {
                         //TODO: Try to send invite to both users
-                        console.log("Inviting users to: " + channelName);
-                        this.inviteToChannel(channelName, '5c15446bbf35ae4057111111');
-                        this.inviteToChannel(channelName, '5c15446bbf35ae4057222222');
+                        console.log("Inviting users to: " + req.body.channelName);
+                        this.inviteToChannel(req.body.channelName, '5c15446bbf35ae4057111111');
+                        this.inviteToChannel(req.body.channelName, '5c15446bbf35ae4057222222');
                     }
 
                     //Returning 5xx error
-                    return res.status(501).json({ status: 'Failed to create channel. ' + error.message + ". Error: " + error.code });
+                    return res.status(501).json({
+                        status: 'Failed to create channel. ' + error.message + ". Error: " + error.code
+                    });
                 });
 
-                if(newChannel){
-                    this.inviteToChannel(channelName, '5c15446bbf35ae4057111111');
-                    this.inviteToChannel(channelName, '5c15446bbf35ae4057222222');
-                    return res.status(201).json({ status: 'Twilio channel created' });
+            //The channel was created
+            if (newChannel) {
+                //send invites
+                this.sendInvites() //
+                if (!req.body.inviteList) {
+                    //return res.status(400).send('The body does not contain a channel name');]
+                    console.log("No inviteList was added");
                 }
+                return res.status(201).json({
+                    status: 'Twilio channel created'
+                });
+            }
+        }
+        async sendInvites() {
+            //If there are any users in the body of the post request
+            if (req.body.inviteList) {
+                var i;
+                for (i = 0; i < req.body.inviteList.length; i++) {
+                    console.log(req.body.inviteList[i]);
+                    this.inviteToChannel(req.body.channelName, req.body.inviteList[i]);
+                }
+            }
         }
     }
 
@@ -101,14 +126,14 @@ exports.createTwilioChannel = async function (req, res) {
     //test.updateChannelData('CHc71753c96f8545d6a99d0e054cc48485');
     //test.inviteToChannel('CH8a8d7a942f594416a080ccbb7a809e74', '5c15446bbf35ae4057111111');
     //test.inviteToChannel('CH8a8d7a942f594416a080ccbb7a809e74', '5c15446bbf35ae4057222222');
-    
+
     try {
-        return test.createChannel(req.body.channelName);
+        return test.createChannel();
     } catch (err) {
         return res.sendStatus(500);
     }
 
-    
+
 }
 
 
@@ -135,5 +160,7 @@ exports.createTwilioUser = async function (req, res) {
     } catch (err) {
         return res.sendStatus(500);
     }
-    return res.status(201).json({ status: 'Twilio user created' });
+    return res.status(201).json({
+        status: 'Twilio user created'
+    });
 };
